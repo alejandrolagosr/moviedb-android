5ded545abe7ab9c28dbe781e2fea87ba4854e60e
Alejandro Lagos <flagos.rd@gmail.com>
2021-05-09 15:08:23 -0500

    Added ImageViewExtensions.kt

---
+++ android/app/src/main/java/com/backbase/assignment/ui/extensions/ImageViewExtensions.kt
@@ -0,0 +1,64 @@
+package com.backbase.assignment.ui.extensions
+
+import android.graphics.drawable.Drawable
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
+import com.backbase.assignment.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.facebook.shimmer.ShimmerFrameLayout
+
+private const val NO_RESOURCE_ID = 0
+private const val SHIMMER_RUNNABLE_DELAY = 50L
+
+fun ImageView.loadImageFromUrl(url: String, @DrawableRes placeHolder: Int = NO_RESOURCE_ID, @DrawableRes errorImage: Int = NO_RESOURCE_ID) {
+    Glide.with(this)
+        .load(url)
+        .placeholder(placeHolder)
+        .error(errorImage)
+        .into(this)
+}
+
+fun ImageView.loadImageFromUrlWithShimmering(url: String, shimmer: ShimmerFrameLayout) {
+    Glide.with(context)
+        .load(url)
+        .placeholder(R.color.white_alpha_60_percent)
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .error(NO_RESOURCE_ID)
+        .listener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    shimmer.apply {
+                        visibility = View.GONE
+                        stopShimmer()
+                    }
+                }, SHIMMER_RUNNABLE_DELAY)
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    shimmer.apply {
+                        visibility = View.GONE
+                        stopShimmer()
+                    }
+                }, SHIMMER_RUNNABLE_DELAY)
+                return false
+            }
+        })
+        .centerCrop()
+        .into(this)
+}
