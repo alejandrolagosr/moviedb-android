e763d1e38392a850ce377e66ae089a647cd75e4c
Alejandro Lagos <flagos.rd@gmail.com>
2021-05-17 11:36:58 -0500

    Added Now Playing movies use case tests

--- android/app/src/test/java/com/flagos/domain/usecase/MovieDbUseCaseTest.kt
+++ android/app/src/test/java/com/flagos/domain/usecase/MovieDbUseCaseTest.kt
@@ -2,8 +2,10 @@ package com.flagos.domain.usecase
 
 import com.flagos.domain.detail.model.MovieDetailItem
 import com.flagos.domain.detail.model.MovieErrorItem
+import com.flagos.domain.home.model.MoviesItem
 import com.flagos.domain.repository.MovieDbRepository
 import com.flagos.domain.retrofit.NetworkResponse
+import com.flagos.domain.retrofit.NetworkResponse.*
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
@@ -29,67 +31,121 @@ class MovieDbUseCaseTest {
 
     private val movieDbRepository = mock(MovieDbRepository::class.java)
     private val movieDetailItem = mock(MovieDetailItem::class.java)
+    private val moviesItem = mock(MoviesItem::class.java)
     private val movieErrorItem = mock(MovieErrorItem::class.java)
     private val ioException = mock(IOException::class.java)
     private val throwable = mock(Throwable::class.java)
+
     private val movieId = 1324
 
-    private lateinit var playingNowUseCase: MovieDbUseCase
+    private lateinit var movieDbUseCase: MovieDbUseCase
 
     @Before
     fun setUp() {
-        playingNowUseCase = MovieDbUseCase(movieDbRepository, testDispatcher)
+        movieDbUseCase = MovieDbUseCase(movieDbRepository, testDispatcher)
     }
 
     @Test
     fun `Given movie detail return Success, when movie detail is called, then show normal ui`() = runBlockingTest(testDispatcher) {
         //Given
-        whenever(movieDbRepository.getMovieDetail(movieId)).thenReturn(flowOf(NetworkResponse.Success(movieDetailItem)))
+        whenever(movieDbRepository.getMovieDetail(movieId)).thenReturn(flowOf(Success(movieDetailItem)))
 
         //When
-        val result = playingNowUseCase.getMovieDetail(movieId).first()
+        val result = movieDbUseCase.getMovieDetail(movieId).first()
 
         //Then
-        assertThat(result, IsInstanceOf(NetworkResponse.Success::class.java))
-        assertThat((result as NetworkResponse.Success<*>).body, Is(equalTo(movieDetailItem)))
+        assertThat(result, IsInstanceOf(Success::class.java))
+        assertThat((result as Success<*>).body, Is(equalTo(movieDetailItem)))
     }
 
     @Test
     fun `Given movie detail return ApiError, when movie detail is called, then show error`() = runBlockingTest(testDispatcher) {
         //Given
-        whenever(movieDbRepository.getMovieDetail(movieId)).thenReturn(flowOf(NetworkResponse.ApiError(movieErrorItem)))
+        whenever(movieDbRepository.getMovieDetail(movieId)).thenReturn(flowOf(ApiError(movieErrorItem)))
 
         //When
-        val result = playingNowUseCase.getMovieDetail(movieId).first()
+        val result = movieDbUseCase.getMovieDetail(movieId).first()
 
         //Then
-        assertThat(result, IsInstanceOf(NetworkResponse.ApiError::class.java))
-        assertThat((result as NetworkResponse.ApiError<*>).body, Is(equalTo(movieErrorItem)))
+        assertThat(result, IsInstanceOf(ApiError::class.java))
+        assertThat((result as ApiError<*>).body, Is(equalTo(movieErrorItem)))
     }
 
     @Test
     fun `Given movie detail return NetworkError, when movie detail is called, then show error`() = runBlockingTest(testDispatcher) {
         //Given
-        whenever(movieDbRepository.getMovieDetail(movieId)).thenReturn(flowOf(NetworkResponse.NetworkError(ioException)))
+        whenever(movieDbRepository.getMovieDetail(movieId)).thenReturn(flowOf(NetworkError(ioException)))
 
         //When
-        val result = playingNowUseCase.getMovieDetail(movieId).first()
+        val result = movieDbUseCase.getMovieDetail(movieId).first()
 
         //Then
-        assertThat(result, IsInstanceOf(NetworkResponse.NetworkError::class.java))
-        assertThat((result as NetworkResponse.NetworkError).error, Is(equalTo(ioException)))
+        assertThat(result, IsInstanceOf(NetworkError::class.java))
+        assertThat((result as NetworkError).error, Is(equalTo(ioException)))
     }
 
     @Test
     fun `Given movie detail return UnknownError, when movie detail is called, then show error`() = runBlockingTest(testDispatcher) {
         //Given
-        whenever(movieDbRepository.getMovieDetail(movieId)).thenReturn(flowOf(NetworkResponse.UnknownError(throwable)))
+        whenever(movieDbRepository.getMovieDetail(movieId)).thenReturn(flowOf(UnknownError(throwable)))
 
         //When
-        val result = playingNowUseCase.getMovieDetail(movieId).first()
+        val result = movieDbUseCase.getMovieDetail(movieId).first()
 
         //Then
-        assertThat(result, IsInstanceOf(NetworkResponse.UnknownError::class.java))
-        assertThat((result as NetworkResponse.UnknownError).error, Is(equalTo(throwable)))
+        assertThat(result, IsInstanceOf(UnknownError::class.java))
+        assertThat((result as UnknownError).error, Is(equalTo(throwable)))
+    }
+
+    @Test
+    fun `Given playing now movies return Success, when playing now is called, then show normal ui`() = runBlockingTest(testDispatcher) {
+        //Given
+        whenever(movieDbRepository.getPlayingNowMovies()).thenReturn(flowOf(Success(moviesItem)))
+
+        //When
+        val result = movieDbUseCase.getPlayingNowMovies().first()
+
+        //Then
+        assertThat(result, IsInstanceOf(Success::class.java))
+        assertThat((result as Success<*>).body, Is(equalTo(moviesItem)))
+    }
+
+    @Test
+    fun `Given playing now movies return ApiError, when movie detail is called, then show error`() = runBlockingTest(testDispatcher) {
+        //Given
+        whenever(movieDbRepository.getPlayingNowMovies()).thenReturn(flowOf(ApiError(movieErrorItem)))
+
+        //When
+        val result = movieDbUseCase.getPlayingNowMovies().first()
+
+        //Then
+        assertThat(result, IsInstanceOf(ApiError::class.java))
+        assertThat((result as ApiError<*>).body, Is(equalTo(movieErrorItem)))
+    }
+
+    @Test
+    fun `Given playing now movies return NetworkError, when movie detail is called, then show error`() = runBlockingTest(testDispatcher) {
+        //Given
+        whenever(movieDbRepository.getPlayingNowMovies()).thenReturn(flowOf(NetworkError(ioException)))
+
+        //When
+        val result = movieDbUseCase.getPlayingNowMovies().first()
+
+        //Then
+        assertThat(result, IsInstanceOf(NetworkError::class.java))
+        assertThat((result as NetworkError).error, Is(equalTo(ioException)))
+    }
+
+    @Test
+    fun `Given playing now movies return UnknownError, when playing now movies is called, then show error`() = runBlockingTest(testDispatcher) {
+        //Given
+        whenever(movieDbRepository.getPlayingNowMovies()).thenReturn(flowOf(UnknownError(throwable)))
+
+        //When
+        val result = movieDbUseCase.getPlayingNowMovies().first()
+
+        //Then
+        assertThat(result, IsInstanceOf(UnknownError::class.java))
+        assertThat((result as UnknownError).error, Is(equalTo(throwable)))
     }
 }
